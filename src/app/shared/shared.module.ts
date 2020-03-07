59cfde524996a6a277bc882a75c44ce727c9b860
k.kwakwamark@gmail.com <caesarion@Marks-MacBook-Pro.local>
2020-03-07 14:31:30 +0000

    added angular-material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [NavComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
   ],
   exports: [
     NavComponent
