c29f451cad7aebf5df9478d010cf45d497ed907c
k.kwakwamark@gmail.com <caesarion@Marks-MacBook-Pro.local>
2020-03-07 16:45:35 +0000

    added goToLink to tile comp, added animations to playlist comp

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,11 +1,27 @@
 import { Playlist } from './models/Playlist';
 import { PlaylistService } from './service/playlist.service';
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.component.html',
-  styleUrls: ['./playlist.component.scss']
+  styleUrls: ['./playlist.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* <=> *', [
+        query(':enter',
+          [style({ opacity: 0 }),
+            stagger('150ms', animate('600ms ease-out', style({ opacity: 1 })))],
+          { optional: true }
+        ),
+        query(':leave',
+          animate('200ms', style({ opacity: 0 })),
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 export class PlaylistComponent implements OnInit {
   playlists: Playlist[] = [];
--- src/app/shared/tile/tile.component.ts
+++ src/app/shared/tile/tile.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
-  styleUrls: ['./tile.component.scss']
+  styleUrls: ['./tile.component.scss'],
 })
 export class TileComponent implements OnInit {
   @Input() img;
+  @Input() link = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  goToPage(): void {
+    window.open(this.link);
   }
 
 }
